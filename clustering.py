7769e2a5ade02a9adda7b3159679c56c243d2913
Krishang Jain <krishangjain2011@gmail.com>
2026-02-01 19:26:01 +0530

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -1,19 +1,26 @@
-from sklearn.cluster import DBSCAN
 import numpy as np
+from sklearn.cluster import DBSCAN
 
 class FaceCluster:
     def __init__(self):
         self.embeddings = []
         self.model = None
 
-    def train(self, embeddings):
-        self.embeddings = np.array(embeddings)
-        self.model = DBSCAN(eps=0.6, min_samples=3, metric="euclidean")
+    def train(self):
+        if len(self.embeddings) < 3:
+            return
+        self.model = DBSCAN(eps=0.08, min_samples=3)
         self.model.fit(self.embeddings)
 
-    def is_known(self, new_embedding):
+    def add_embedding(self, emb):
+        self.embeddings.append(emb)
+        self.train()
+
+    def is_known(self, emb):
         if self.model is None:
             return False
 
-        distances = np.linalg.norm(self.embeddings - new_embedding, axis=1)
-        return min(distances) < 0.6
+        distances = np.linalg.norm(
+            np.array(self.embeddings) - emb, axis=1
+        )
+        return distances.min() < 0.08
